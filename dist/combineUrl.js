a44ebc5b3b06a00ad2bea3e5024139018dc6c47f
contra <yo@contra.io>
2018-10-26 19:23:47 -0400

    combineUrl util

---
+++ dist/combineUrl.js
@@ -0,0 +1,30 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _url = require('url');
+
+var _url2 = _interopRequireDefault(_url);
+
+var _qs = require('qs');
+
+var _qs2 = _interopRequireDefault(_qs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function (endpoint, query) {
+  var parsed = _url2.default.parse(endpoint);
+
+  var q = _qs2.default.stringify(Object.assign({}, _qs2.default.parse(parsed.query, { strictNullHandling: true }), query), { strictNullHandling: true });
+
+  return _url2.default.format({
+    protocol: parsed.protocol,
+    auth: parsed.auth,
+    port: parsed.port,
+    host: parsed.host,
+    pathname: parsed.pathname,
+    search: q
+  });
+};
+
+module.exports = exports['default'];
